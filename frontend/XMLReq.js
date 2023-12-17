35a9ba9fedfa8b150f0c3491ec1cfdc90b33cfb3
Nacho Bono <107893519+bonhoff@users.noreply.github.com>
2023-12-17 13:53:03 -0800

    XML Request JavaScript

---
+++ frontend/XMLReq.js
@@ -0,0 +1,33 @@
+class XMLReq {
+    constructor(url) {
+        this.url = url;
+    }
+
+    #xmlreq(method, data = "") {
+        let dataPromise = new Promise((res, rej) => {
+            let xhr = new XMLHttpRequest();
+            xhr.onload = () => {
+                if (xhr.status == 200) {
+                    console.log(xhr);
+                    res(xhr.responseText);
+                } else {
+                    rej(xhr.statusText);
+                }
+            }
+            xhr.open(method, this.url);
+            (data != "") ? xhr.send(data) : xhr.send();
+        })
+        return dataPromise;
+    }
+
+    Post(data) {
+        return this.#xmlreq("POST", data);
+    }
+
+    Get() {
+        return this.#xmlreq("GET");
+    }
+
+}
+//export
+export default XMLReq;
