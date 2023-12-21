809e4a24d08d818908b31c150d4f39a2dfe14797
Nacho Bono <107893519+bonhoff@users.noreply.github.com>
2023-12-20 23:17:55 -0800

    Update roomStatus.php

--- backend/roomStatus.php
+++ backend/roomStatus.php
@@ -3,9 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 include("./roomClass.php");
 
-session_start();
-include 'databaseInfo.php';
 
+session_start();
+include("./databaseInfo.php");
 
 // $DATABASE_HOST = "localhost";
 // $DATABASE_USER = "root";
@@ -21,13 +21,16 @@ function getData(){
     return "SELECT * FROM rooms";           //sql query
 }
 
+// function getAvailabiliyty(){
+//     return "SELECT a.occupied FROM availability a, rooms r WHERE a.rid = r.rid";
+// }
+
 $roomData = query($connection, getData());      //get the room data 
 
 while ($row = mysqli_fetch_assoc($roomData)) {  //while to iterate in each row and convert resultset SQL to an Array(associate Array)
     $room = new Room(
-        $row["id"],             //get the id from the rows
-        $row["type"],           //"" type ""
-        $row["availability"],    //"" availability ""
+        $row["rid"],             //get the id from the rows
+        $row["room_type"],           //"" type ""
         $row["status"],         //"" status ""
         $row["details"],     //"" details ""
     );
